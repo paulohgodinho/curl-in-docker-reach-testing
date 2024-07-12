b84020c91d336955e53f9ab94c6f02b6c7b7dba7
Paulo Godinho <paulo@godinhosilva.com.br>
2024-07-12 00:43:15 -0300

    Using bash instead of sh

--- run.sh
+++ run.sh
@@ -6,16 +6,16 @@ mkdir "./results"
 # Default Net
 fileName="1-default_network.txt"
 touch "./results/$fileName"
-docker run -v ${PWD}/results/$fileName:/result.txt -v ${PWD}/curl_reach_test.sh:/reach_test.sh docker.io/paulohgodinho/debianwithcurl:latest sh reach_test.sh
+docker run -v ${PWD}/results/$fileName:/result.txt -v ${PWD}/curl_reach_test.sh:/reach_test.sh docker.io/paulohgodinho/debianwithcurl:latest bash reach_test.sh
 
 # New Bridge
 docker network create -d bridge temp-bridge
 fileName="2-new-bridge.txt"
 touch "./results/$fileName"
-docker run --network temp-bridge -v ${PWD}/results/$fileName:/result.txt -v ${PWD}/curl_reach_test.sh:/reach_test.sh docker.io/paulohgodinho/debianwithcurl:latest sh reach_test.sh
+docker run --network temp-bridge -v ${PWD}/results/$fileName:/result.txt -v ${PWD}/curl_reach_test.sh:/reach_test.sh docker.io/paulohgodinho/debianwithcurl:latest bash reach_test.sh
 docker network remove temp-bridge
 
 # Wings Network
 fileName="3-wings-network.txt"
 touch "./results/$fileName"
-docker run --network pterodactyl_nw -v ${PWD}/results/$fileName:/result.txt -v ${PWD}/curl_reach_test.sh:/reach_test.sh docker.io/paulohgodinho/debianwithcurl:latest sh reach_test.sh
+docker run --network pterodactyl_nw -v ${PWD}/results/$fileName:/result.txt -v ${PWD}/curl_reach_test.sh:/reach_test.sh docker.io/paulohgodinho/debianwithcurl:latest bash reach_test.sh
